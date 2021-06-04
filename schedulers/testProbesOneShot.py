e3888d7591a62db5ca0bcc20a3a2d8081053b379
Julien <jbuisine59@gmail.com>
2021-06-04 17:13:00 +0200

    correction name scheduler probes

--- schedulers/testProbesOneShot.py
+++ schedulers/testProbesOneShot.py
@@ -6,7 +6,7 @@ from procset import ProcSet
 from itertools import islice
 
 
-class TestProbOneShot (BatsimScheduler):
+class TestProbesOneShot (BatsimScheduler):
 
     def onAfterBatsimInit(self):
         self.nb_completed_jobs = 0
