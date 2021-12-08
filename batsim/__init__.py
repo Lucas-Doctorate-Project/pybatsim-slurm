43cbf14f9e7d492e5190a9b56315ecbbf4d9b68e
Raphaël Bleuse <raphael.bleuse@inria.fr>
2021-12-08 17:49:16 +0100

    Single source the package version
    The version of the package is defined once in `pyproject.toml`.
    The two packages `batsim` and `schedulers` now rely on `importlib` to
    retrieve the version.
    Refer to [1] for details.
    
    [1]: https://packaging.python.org/en/latest/guides/single-sourcing-package-version/

--- batsim/__init__.py
+++ batsim/__init__.py
@@ -1,1 +1,10 @@
-from batsim._version import __version__
+import sys
+
+if sys.version_info >= (3, 8):
+    from importlib import metadata
+else:
+    import importlib_metadata as metadata
+
+__version__ = metadata.version('pybatsim')
+
+__all__ = [__version__]
