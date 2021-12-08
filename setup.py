f3bbb4f33f1746d6699953fd06bde5f94293c5d2
Raphaël Bleuse <raphael.bleuse@inria.fr>
2021-12-08 15:20:51 +0100

    Remove custom setuptools command to format code

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ requirements = [
 
 setup_requirements = [
     "coverage",
-    "autopep8",
     "ipdb",
     "docopt"
 ]
@@ -93,34 +92,6 @@ class DocCommand(UserCommand):
             new=new)
 
 
-class FormatCommand(UserCommand):
-
-    description = 'Format the source code'
-    user_options = [
-        ('path=', 'p', 'start directory or file'),
-    ]
-
-    def initialize_options(self):
-        self.path = "."
-
-    def finalize_options(self):
-        if self.path is None:
-            raise Exception("Parameter --path is missing")
-        elif not os.path.exists(self.path):
-            raise Exception("Path {} does not exist".format(self.path))
-
-    def run(self):
-        self.run_external_command(
-            "autopep8",
-            "-i",
-            "-r",
-            "-j",
-            "0",
-            "-aaaaaa",
-            "--experimental",
-            self.path)
-
-
 f = open("./README.rst")
 read_me = f.read().strip()
 f.close()
@@ -159,7 +130,6 @@ setup(
     },
     cmdclass={
         'test': TestCommand,
-        'format': FormatCommand,
         'doc': DocCommand,
     },
 )
