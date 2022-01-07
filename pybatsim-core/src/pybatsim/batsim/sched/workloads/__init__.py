71113e3b23c4f2780d403d7df14b33131527ead5
Raphaël Bleuse <raphael.bleuse@inria.fr>
2022-01-07 09:53:46 +0100

    Move functional API in `pybatsim-functional` package

--- pybatsim-core/src/pybatsim/batsim/sched/workloads/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-    batsim.sched.workloads
-    ~~~~~~~~~~~~~~~~~~~~~~
-
-    This modules provides utilities to generate workloads from workload models.
-
-"""
-
-from .workloads import JobDescription, WorkloadDescription, generate_workload
--- pybatsim-core/src/pybatsim/batsim/sched/workloads/models/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-    batsim.sched.workloads.models
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    This package provides tools to generate workloads from models.
-
-"""
