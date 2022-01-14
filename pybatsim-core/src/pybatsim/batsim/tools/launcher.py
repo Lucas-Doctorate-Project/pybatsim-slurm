ff701e59869aacf1019abd4f210d6f031a04909f
Raphaël Bleuse <raphael.bleuse@inria.fr>
2022-01-14 17:32:54 +0100

    Remove pybatsim.batsim.cmds module
    The module provided two scripts to launch schedulers:
    - `pybatsim.batsim.cmds.launcher:main` has been replaced by
      `pybatsim.cmdline:main`
    - `pybatsim.batsim.cmds.experiments:main` is an experiments orchestrator
      and is out of the scope of this project: see [1] for an alternative
    
    [1]: https://gitlab.inria.fr/batsim/batexpe

--- pybatsim-core/src/pybatsim/batsim/tools/launcher.py
+++ pybatsim-core/src/pybatsim/batsim/tools/launcher.py
@@ -9,82 +9,8 @@ import json
 import sys
 import time
 from datetime import timedelta
-import importlib.util
-import os.path
 
 from pybatsim.batsim.batsim import Batsim
-
-
-def module_to_class(module):
-    """
-    transform fooBar to FooBar
-    """
-    return (module[0]).upper() + module[1:]
-
-
-def filename_to_module(fn):
-    return str(fn).split(".")[0]
-
-
-def instanciate_scheduler(name, options):
-    # A scheduler module in the package "pybatsim.schedulers" is expected.
-    if "." not in name and "/" not in name:
-        my_module = name  # filename_to_module(my_filename)
-        my_class = module_to_class(my_module)
-
-        # load module(or file)
-        package = __import__('pybatsim.schedulers', fromlist=[my_module])
-        if my_module not in package.__dict__:
-            print("No such scheduler (module file not found).")
-            sys.exit(1)
-        if my_class not in package.__dict__[my_module].__dict__:
-            print("No such scheduler (class within the module file not found).")
-            sys.exit(1)
-        # load the class
-        scheduler_non_instancied = package.__dict__[
-            my_module].__dict__[my_class]
-
-    # A full file path to the scheduler is expected
-    else:
-        # Add path to allow relative imports in the scheduler implementation
-        sys.path.insert(0, os.path.abspath(os.path.dirname(name)))
-        sys.path.insert(
-            0, os.path.abspath(
-                os.path.dirname(
-                    os.path.dirname(name))))
-
-        package_path = os.path.split(os.path.dirname(name))[1]
-        if package_path:
-            package_path = [package_path]
-        else:
-            package_path = []
-        module_name = os.path.basename(name).split(".")[0]
-        module_path = ".".join(package_path + [module_name])
-
-        my_class = module_to_class(module_name)
-
-        # Try to load the module with the outer package
-        try:
-            mod = importlib.import_module(module_path)
-        # Try to load only the module as fallback
-        except ModuleNotFoundError:
-            spec = importlib.util.spec_from_file_location(
-                "pybatsim.schedulers." + module_name, name)
-            mod = importlib.util.module_from_spec(spec)
-            spec.loader.exec_module(mod)
-
-        del sys.path[1]
-        del sys.path[0]
-
-        try:
-            scheduler_non_instancied = mod.__dict__[my_class]
-        except KeyError:
-            print("Module {} contains no scheduler named {}".format(
-                mod, my_class))
-            sys.exit(1)
-
-    scheduler = scheduler_non_instancied(options)
-    return scheduler
 
 
 def launch_scheduler(scheduler,
