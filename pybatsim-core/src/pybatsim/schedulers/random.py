5045b94383b921703a05e5a2f4936fd5ff3340e8
Raphaël Bleuse <raphael.bleuse@inria.fr>
2025-04-18 13:59:36 +0200

    Make random scheduler an instance of Scheduler

--- pybatsim-core/src/pybatsim/schedulers/random.py
+++ pybatsim-core/src/pybatsim/schedulers/random.py
@@ -2,10 +2,10 @@ from random import Random
 
 from procset import ProcSet
 
+from pybatsim.batsim.batsim import Batsim
 from pybatsim.batsim.core import SimulationFeatures
-from pybatsim.batsim.edc import ExternalDecisionComponent
+from pybatsim.batsim.edc import Scheduler
 from pybatsim.batsim.events import (
-    AllStaticJobsHaveBeenSubmittedEvent,
     EDCHelloEvent,
     ExecuteJobEvent,
     JobCompletedEvent,
@@ -14,85 +14,82 @@ from pybatsim.batsim.events import (
     SimulationBeginsEvent,
     SimulationEndsEvent,
 )
+from pybatsim.batsim.job import Job
 
 
-class RandomScheduler(ExternalDecisionComponent):
-    def __init__(self, batsim, options):
-        self._batsim = batsim
-        self._options = options
-        self.scheduling_needed = False
-        self._generator = Random(42)
+class RandomScheduler(Scheduler):
+    waiting_jobs: set[Job] = set()
+    running_jobs: dict[str, Job] = {}
+    idle_resources: ProcSet | None = None
+    cluster_size: int | None = None
+    _generator: Random
 
-        self._batsim.simulation_metadata.requested_features |= \
-                SimulationFeatures.FORWARD_PROFILES_ON_JOB_SUBMISSION
+    def __init__(self, batsim: Batsim, options: str | None = None):
+        super().__init__(batsim, options)
+        self._generator = Random(42)  # TODO: provide mechanism to select seed
 
-        self._batsim.add_event(EDCHelloEvent(self._batsim.time,
-                                             self._batsim.simulation_metadata,
-                                             "RandomSched", "v0.1", ""))
+        self._batsim.simulation_metadata.requested_features |= (
+            SimulationFeatures.FORWARD_PROFILES_ON_JOB_SUBMISSION
+        )
+        edc_hello_event = EDCHelloEvent(
+            timestamp=self._batsim.time,
+            simulation_metadata=self._batsim.simulation_metadata,
+            edc_name=type(self).__name__,
+            edc_version='0.0.0',
+            edc_commit='',
+        )
+        self._batsim.add_event(edc_hello_event)
 
+    def begin_simulation(self, event: SimulationBeginsEvent) -> None:
+        self.cluster_size = event.computation_host_number
+        self.idle_resources = ProcSet((0, self.cluster_size - 1))
 
-    def handle_SimulationBegins(self, event):
-        #TODO: update this if info from SimulationBegins are sent in BatsimHello event
-        self.nb_compute_res = event.computation_host_number
-        self.available_res = ProcSet((0, self.nb_compute_res-1))
-        self.waiting_jobs = set()
-        self.running_jobs = {}
+    def end_simulation(self, event: SimulationEndsEvent) -> None:
+        pass
 
+    def submit_job(self, event: JobSubmittedEvent) -> None:
+        job = event.job
 
-    def handle_msg(self, message):
-        for event in message:
-            match event:
-                case SimulationBeginsEvent():
-                    self.handle_SimulationBegins(event)
-                case SimulationEndsEvent():
-                    pass
-                case JobSubmittedEvent():
-                    self.handle_JobSubmitted(event)
-                case JobCompletedEvent():
-                    self.handle_JobCompleted(event)
-                case AllStaticJobsHaveBeenSubmittedEvent():
-                    pass
-                case _:
-                    raise NotImplementedError(f"Handling of event type '{type(event).__name__}' not implemented")
+        if job.resource_request > self.cluster_size:
+            # trivial reject if job request more resources than cluster_size
+            reject_event = RejectJobEvent(
+                timestamp=self._batsim.time,
+                job_id=job.job_id,
+            )
+            self._batsim.add_event(reject_event)
+        else:
+            self.waiting_jobs.add(job)
 
-        if self.scheduling_needed:
-            self.do_schedule()
-            self.scheduling_needed = False
+    def complete_job(self, event: JobCompletedEvent) -> None:
+        job = self.running_jobs.pop(event.job_id)
+        self.idle_resources |= job.allocation
 
+    def handle_msg(self, msg) -> None:
+        super().handle_msg(msg)
+        self.schedule_jobs()
 
-    def handle_JobSubmitted(self, event):
-        j = event.job
-        if j.resource_request > self.nb_compute_res:
-            self._batsim.add_event(RejectJobEvent(self._batsim.time, j.job_id))
+    def schedule_jobs(self):
+        scheduled_jobs = []
 
-        self.waiting_jobs.add(j)
-        self.scheduling_needed = True
-
-
-    def handle_JobCompleted(self, event):
-        j = self.running_jobs.pop(event.job_id)
-        self.available_res |= j.allocation
-        self.scheduling_needed = True
-
-
-    def do_schedule(self):
-        scheduledJobs = []
-
-        for j in self.waiting_jobs:
-            if j.resource_request <= len(self.available_res):
-                res = self._generator.sample(
-                    list(self.available_res),
-                    j.resource_request
+        for job in self.waiting_jobs:
+            # randomly assign resources if enough resources are idle
+            if job.resource_request <= len(self.idle_resources):
+                allocation = self._generator.sample(
+                    population=list(self.idle_resources),
+                    k=job.resource_request,
                 )
-                print(f"Selected {res} for job {j.job_id}")
-                j.allocation = ProcSet(*res)
+                job.allocation = ProcSet(*allocation)
+                self.idle_resources -= job.allocation
+                scheduled_jobs.append(job)
 
-                self.available_res -= j.allocation
-                scheduledJobs.append(j)
+        # remove scheduled_jobs from waiting_jobs
+        self.waiting_jobs.difference_update(scheduled_jobs)
 
-        for j in scheduledJobs:
-            self.waiting_jobs.remove(j)
-            self.running_jobs[j.job_id] = j
-            self._batsim.add_event(ExecuteJobEvent(self._batsim.time, j))
-
-        self.scheduling_needed = False
+        # send the list of scheduled jobs to Batsim
+        for job in scheduled_jobs:
+            self.running_jobs[job.job_id] = job
+            execute_event = ExecuteJobEvent(
+                timestamp=self._batsim.time,
+                job=job,
+            )
+            self._batsim.add_event(execute_event)
